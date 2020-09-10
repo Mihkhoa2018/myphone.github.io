1a0414395c0b5eb7fb4e1e9eb406db1635aaf74c
Mihkhoa2018 <mihkhoa2018@gmail.com>
2020-09-10 18:46:35 +0700

    web

---
+++ Project/test.js
@@ -0,0 +1,33 @@
+new Vue({
+    el: '#app',
+    data: {
+        list: [{
+                id: 1,
+                img: 'https://didongthongminh.vn/upload_images/2020/08/vmsmart-live-4-xanh.png',
+                title: 'Điện thoại ',
+                price: '12.000.000'
+            }, {
+                id: 2,
+                img: 'https://didongthongminh.vn/upload_images/2019/12/xs-max-gold.png',
+                title: 'Điện thoại ',
+                price: '14.000.000'
+            }, {
+                id: 3,
+                img: 'https://didongthongminh.vn/upload_images/2019/05/note-10-lite-trang.png',
+                title: 'Điện thoại ',
+                price: '16.000.000'
+            }, {
+                id: 4,
+                img: 'https://didongthongminh.vn/upload_images/2019/12/xs-max-gold.png',
+                title: 'Điện thoại ',
+                price: '14.000.000'
+            }, {
+                id: 5,
+                img: 'https://didongthongminh.vn/upload_images/2019/12/xs-max-gold.png',
+                title: 'Điện thoại ',
+                price: '14.000.000'
+            },
+
+        ]
+    }
+})
