7423bd8ecd49445b01748005f77674bdb0d73a38
Mihkhoa2018 <mihkhoa2018@gmail.com>
2020-09-16 23:30:00 +0700

    update

---
+++ assets/js/account.js
@@ -0,0 +1,74 @@
+var vue = new Vue({
+    el: '#signin',
+    data: {
+
+        // Tạo biến quản lý lỗi là mảng rỗng
+        errors1: [],
+        errors2: [],
+
+        // Tạo biến quản lý việc kiếm tra Ràng buộc dữ liệu (validation) hay chưa?
+        // Mặc định là chưa kiểm tra
+        kiemtraloixong: false,
+
+        //tạo giá trị ban đầu
+        user: '',
+        pass: ''
+    },
+    methods: {
+        Kiemtradulieu: function(e) {
+            e.preventDefault();
+
+            //loi chua xay ra
+            this.errors1 = [];
+            this.errors2 = [];
+            this.kiemtraloixong = false;
+
+            //kiem tra user
+            if (this.user == "") {
+                this.errors1.push('Vui lòng nhập tài khoản');
+
+            }
+
+            //kiem tra mat khau
+            if (this.pass == "") {
+                this.errors2.push('Vui lòng nhập mật khẩu');
+            }
+
+            this.kiemtraloixong = true;
+            return false;
+        },
+
+        thongbaoloi1: function() {
+            if (this.kiemtraloixong == false) {
+                return false;
+            }
+            if (this.errors1.length > 0) {
+                return true;
+            }
+            return false;
+        },
+        thongbaoloi2: function() {
+            if (this.kiemtraloixong == false) {
+                return false;
+            }
+            if (this.errors2.length > 0) {
+                return true;
+            }
+            return false;
+        },
+
+        thongbao: function() {
+            // Nếu chưa vượt qua bước kiểm tra lỗi thì không được hiển thị thông báo
+            if (this.dakiemtraloixong == false) {
+                return false;
+            }
+            // Nếu không có bất kỳ lỗi nào (mảng array lỗi là rỗng) => độ dài array == 0)
+            // Không có lỗi => được hiển thị thông báo chào mừng
+            if ((this.errors1.length == 0) || (this.errors2.length == 0)) {
+                return true;
+            }
+            // Mặc định không hiển thị
+            return false;
+        }
+    }
+});
--- test.js
+++ test.js
@@ -1,33 +1,94 @@
-new Vue({
+// Khởi tạo đối tượng (instance) Vue
+var vue = new Vue({
     el: '#app',
     data: {
-        list: [{
-                id: 1,
-                img: 'https://didongthongminh.vn/upload_images/2020/08/vmsmart-live-4-xanh.png',
-                title: 'Điện thoại ',
-                price: '12.000.000'
-            }, {
-                id: 2,
-                img: 'https://didongthongminh.vn/upload_images/2019/12/xs-max-gold.png',
-                title: 'Điện thoại ',
-                price: '14.000.000'
-            }, {
-                id: 3,
-                img: 'https://didongthongminh.vn/upload_images/2019/05/note-10-lite-trang.png',
-                title: 'Điện thoại ',
-                price: '16.000.000'
-            }, {
-                id: 4,
-                img: 'https://didongthongminh.vn/upload_images/2019/12/xs-max-gold.png',
-                title: 'Điện thoại ',
-                price: '14.000.000'
-            }, {
-                id: 5,
-                img: 'https://didongthongminh.vn/upload_images/2019/12/xs-max-gold.png',
-                title: 'Điện thoại ',
-                price: '14.000.000'
-            },
 
-        ]
+        // Tạo biến quản lý lỗi là mảng rỗng
+        errors: [],
+        // Tạo biến quản lý việc kiếm tra Ràng buộc dữ liệu (validation) hay chưa?
+        // Mặc định là chưa kiểm tra
+        dakiemtraloixong: false,
+        // Khởi tạo giá trị ban đầu cho FORM
+        hoten: '',
+        email: '',
+        sodienthoai: '',
+        loinhan: ''
+    },
+    methods: {
+        kiemTraDuLieu: function(e) {
+            // Dừng sự kiện tiếp theo của FORM
+            e.preventDefault();
+            // Trước khi kiểm tra, cần reset lại biến lỗi
+            // => Giả sử như chưa có lỗi xảy ra
+            this.errors = [];
+            this.dakiemtraloixong = false;
+            // Validate Họ tên
+            // Kiểm tra rỗng
+            if (this.hoten == "") {
+                this.errors.push('Vui lòng nhập Họ tên');
+            } else if (this.hoten.length < 5) { // Kiểm tra độ dài
+                this.errors.push('Vui lòng nhập Họ tên 5 ký tự trở lên');
+            }
+            // Validate Email
+            // Kiểm tra rỗng
+            if (this.email == "") {
+                this.errors.push('Vui lòng nhập địa chỉ Email');
+            } else if (this.email.length < 5) { // Kiểm tra độ dài
+                this.errors.push('Vui lòng nhập địa chỉ Email 5 ký tự trở lên');
+            } else if (!this.validateEmail(this.email)) { // Kiểm tra mẫu nhập EMAIL
+                this.errors.push('Vui lòng nhập email đúng định dạng');
+            }
+            // Validate Số điện thoại
+            // Kiểm tra rỗng
+            if (this.sodienthoai == "") {
+                this.errors.push('Vui lòng nhập số điện thoại');
+            } else if (this.sodienthoai.length < 5) { // Kiểm tra độ dài
+                this.errors.push('Vui lòng nhập số điện thoại 5 ký tự trở lên');
+            }
+            // Validate Lời nhắn
+            // Kiểm tra rỗng
+            if (this.loinhan == "") {
+                this.errors.push('Vui lòng nhập lời nhắn');
+            } else if (this.loinhan.length < 5) { // Kiểm tra độ dài
+                this.errors.push('Vui lòng nhập lời nhắn 5 ký tự trở lên');
+            }
+            // Đã kiểm tra lỗi xong
+            this.dakiemtraloixong = true;
+            // Ví dụ demo, ngưng gởi dữ liệu SUBMIT đi
+            // Always return false
+            return false;
+        },
+        validateEmail: function(email) {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                return true;
+            }
+            return false;
+        },
+        hienThiThongBaoLoi: function() {
+            // Nếu chưa vượt qua bước kiểm tra lỗi thì không được hiển thị thông báo
+            if (this.dakiemtraloixong == false) {
+                return false;
+            }
+            // Nếu có bất kỳ lỗi nào (mảng array lỗi không rỗng) => độ dài array > 0)
+            // Có lỗi => được hiển thị thông báo lỗi
+            if (this.errors.length > 0) {
+                return true;
+            }
+            // Nếu không có lỗi thì không được hiển thị thông báo lỗi
+            return false;
+        },
+        hienThiThongBaoChaoMung: function() {
+            // Nếu chưa vượt qua bước kiểm tra lỗi thì không được hiển thị thông báo
+            if (this.dakiemtraloixong == false) {
+                return false;
+            }
+            // Nếu không có bất kỳ lỗi nào (mảng array lỗi là rỗng) => độ dài array == 0)
+            // Không có lỗi => được hiển thị thông báo chào mừng
+            if (this.errors.length == 0) {
+                return true;
+            }
+            // Mặc định không hiển thị
+            return false;
+        }
     }
-})
+});
